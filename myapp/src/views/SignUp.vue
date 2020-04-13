6d09627c5dcc116a36612cee5dd2e8a75faf8e9a
dianaitr <dianatorresrojas@hotmail.com>
2020-04-13 15:55:25 -0500

    Changing login and signup

--- myapp/src/views/SignUp.vue
+++ myapp/src/views/SignUp.vue
@@ -1,57 +1,79 @@
 <template>
     <div>
-        <!-- <h2>Register</h2>
-        <form @submit.prevent="handleSubmit">
-            <div class="form-group">
-                <label for="firstName">First Name</label>
-                <input type="text" v-model="user.firstName" v-validate="'required'" name="firstName" class="form-control" :class="{ 'is-invalid': submitted && errors.has('firstName') }" />
-                <div v-if="submitted && errors.has('firstName')" class="invalid-feedback">{{ errors.first('firstName') }}</div>
-            </div>
-            <div class="form-group">
-                <label for="lastName">Last Name</label>
-                <input type="text" v-model="user.lastName" v-validate="'required'" name="lastName" class="form-control" :class="{ 'is-invalid': submitted && errors.has('lastName') }" />
-                <div v-if="submitted && errors.has('lastName')" class="invalid-feedback">{{ errors.first('lastName') }}</div>
-            </div>
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" v-model="user.username" v-validate="'required'" name="username" class="form-control" :class="{ 'is-invalid': submitted && errors.has('username') }" />
-                <div v-if="submitted && errors.has('username')" class="invalid-feedback">{{ errors.first('username') }}</div>
-            </div>
-            <div class="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" v-model="user.password" v-validate="{ required: true, min: 6 }" name="password" class="form-control" :class="{ 'is-invalid': submitted && errors.has('password') }" />
-                <div v-if="submitted && errors.has('password')" class="invalid-feedback">{{ errors.first('password') }}</div>
-            </div>
-            <div class="form-group">
-                <button class="btn btn-primary" :disabled="status.registering">Register</button>
-                <img v-show="status.registering" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                <router-link to="/login" class="btn btn-link">Cancel</router-link>
-            </div>
-        </form> -->
-        <h1>Sign up</h1>
+        <h2>Sign Up</h2>
+        <form @submit.prevent="saveUser">
+            <v-text-field class="margen" v-model="nombre" label="Name"></v-text-field>
+            <v-text-field class="margen" v-model="apellido" label="Last Name"></v-text-field>
+            <v-text-field class="margen" v-model="activo" label="Active"></v-text-field>
+            <v-text-field class="margen" v-model="email" label="Email"></v-text-field>
+           <v-text-field v-model="contrasena"
+            :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="show2 ? 'text' : 'password'"
+            name="input-10-2"
+            label="Visible"
+            hint="At least 8 characters"
+            value="wqfasds"
+            class="input-group--focused"
+            @click:append="show2 = !show2"
+          ></v-text-field>
+
+            <v-btn type="submit" color="green" class="margen">REGISTER</v-btn>
+        </form>
+        
     </div>
 </template>
 
 <script>
-
+import db from "../config/firebaseInit";
 export default {
-    /* data () {
+    data () {
         return {
             user: {
-                firstName: '',
-                lastName: '',
-                username: '',
-                password: ''
+                nombre: '',
+                apellido: '',
+                activo: '',
+                contrasena: '',
+                email:'',
+                numero_mensajes: 0,
+                id_mensajes: null,
+                id_subjets: null
             },
-            submitted: false
+            show1: false,
+            show2: true,
+            show3: false,
+            show4: false,
+            password: 'Password',
+            rules: {
+                required: value => !!value || 'Required.',
+                min: v => v.length >= 8 || 'Min 8 characters',
+                emailMatch: () => ('The email and password you entered don\'t match'),
+        },
         }
     },
-    computed: {
-       
-    },
     methods: {
+        saveUser(){
+            if(this.nombre == null || this.apellido == null || this.activo == null || this.email == null || this.contrasena == null){
+            alert("Llene todos los campos")
+        }else{
+            db.collection("usuarios")
+        .add({
+          nombre: this.nombre,
+          apellido: this.apellido,
+          activo: this.activo,
+          email: this.email,
+          contrasena: this.contrasena
+        })
+        .catch(error => console.log(error));
+        }
+        }
         
-        
-    } */
+    } 
 };
 </script>
+
+<style>
+.margen{
+    margin: 1%;
+}
+</style>
